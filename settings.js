58dd89b56a97362e3ebee32f9e4dabd3f4bffb79
IzharDeveloper <159452925+IzharDevelop@users.noreply.github.com>
2024-10-14 20:33:35 +0700

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,7 @@
+global.prefa = ['','!','.',',','🐤','🗿']
+
+global.owner = ['6283822847501']
+global.botname = 'TenSix'
+global.baileys1 = require('@whiskeysockets/baileys') 
+global.sticker1 = "Cyber orbox"
+global.sticker2 = "🌜"
